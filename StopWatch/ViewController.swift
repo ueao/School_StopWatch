8b34ce6e02e042c920ed2a39a0fdf34272bb917e
Sakaue Aoi <aoiue.w.bbb@gail.com>
2016-11-11 16:48:29 +0900

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Aoi Sakaue on 2016/11/11.
+//  Copyright © 2016年 Sakaue Aoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
